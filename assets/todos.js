2483dc720f9cc4544f643a656045076f8cac4634
Nick Martello <thenickmartello@gmail.com>
2020-04-16 14:27:39 -0400

    favicon added

--- assets/todos.js
+++ assets/todos.js
@@ -15,3 +15,6 @@ $("input[type='text']").keypress(function(e){
         $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 });
+$(".fa-pencil").click(function(){
+    $("input[type='text']").fadeToggle()
+})
