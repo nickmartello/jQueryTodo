f411da920870f3ff295c337d90ec58deda44718b
Nick Martello <thenickmartello@gmail.com>
2020-04-16 13:16:59 -0400

    input added

--- assets/todos.js
+++ assets/todos.js
@@ -1,10 +1,17 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     e.stopPropagation();
-})
+});
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        let todoText = $(this).val();
+        $("ul").append("<li><span>X</span> " + todoText + "<li>")
+    }
+});
